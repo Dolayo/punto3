5f7a7088954decf58b4e1cc359e6ced9c3fbb1aa
Dolayo <d.olayo.andres@gmail.com>
2020-10-01 13:12:52 +0200

    funciona

--- nodes/turtle_tf2_listener.py
+++ nodes/turtle_tf2_listener.py
@@ -8,8 +8,33 @@ import turtlesim.srv
 from std_srvs.srv import Empty
 
 def handler(req):
-    global flag_s
-    flag_s = True
+
+    global flag
+    spawner(4, 2, 0, turtle_name)
+    while not rospy.is_shutdown():
+        try:
+            
+            trans = tfBuffer.lookup_transform(turtle_name, 'turtle1', rospy.Time())
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rate.sleep()
+            continue
+
+        msg = geometry_msgs.msg.Twist()
+
+        x_pose = trans.transform.translation.x
+        y_pose = trans.transform.translation.y
+
+        if (math.sqrt(x_pose ** 2 + y_pose ** 2)) < 1:
+            flag = True
+
+        if(flag):    
+            msg.angular.z = 4 * math.atan2(y_pose, x_pose)
+            msg.linear.x = 0.5 * math.sqrt(x_pose ** 2 + y_pose ** 2)
+
+        turtle_vel.publish(msg)
+
+        rate.sleep()
+
     return []
 
 if __name__ == '__main__':
@@ -28,35 +53,11 @@ if __name__ == '__main__':
     rate = rospy.Rate(10.0)
 
     flag = False
-    flag_s = False
 
     
     server = rospy.Service('start_turtlesim_snake', Empty, handler)
+    rospy.spin()
 
-    if(flag_s):
-        while not rospy.is_shutdown():
-            try:
-                spawner(4, 2, 0, turtle_name)
-                trans = tfBuffer.lookup_transform(turtle_name, 'turtle1', rospy.Time())
-            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-                rate.sleep()
-                continue
-
-            msg = geometry_msgs.msg.Twist()
-
-            x_pose = trans.transform.translation.x
-            y_pose = trans.transform.translation.y
-
-            if (math.sqrt(x_pose ** 2 + y_pose ** 2)) < 1:
-                flag = True
-
-            if(flag):    
-                msg.angular.z = 4 * math.atan2(y_pose, x_pose)
-                msg.linear.x = 0.5 * math.sqrt(x_pose ** 2 + y_pose ** 2)
-
-            turtle_vel.publish(msg)
-
-            rate.sleep()
     
 
     
